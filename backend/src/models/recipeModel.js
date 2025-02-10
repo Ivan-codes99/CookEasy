f3b42532e02b4e4ef8df99063566a8cabc2c2c15
crivilla <crivilla@gatech.edu>
2025-02-10 18:33:36 -0500

    Added models

---
+++ backend/src/models/recipeModel.js
@@ -0,0 +1,10 @@
+// const mongoose = require('mongoose');
+//
+// const recipeSchema = new mongoose.Schema({
+//     title: { type: String, required: true },
+//     ingredients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Ingredient' }],
+//     instructions: { type: String, required: true },
+// });
+//
+// const Recipe = mongoose.model('Recipe', recipeSchema);
+// module.exports = Recipe;
