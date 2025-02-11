b3019b1ba62c594dbbc71c81fd9e823269a3b843
Ivan Figueroa <Ivanfiggomez@gmail.com>
2025-02-11 17:14:05 -0500

    minor stuff

--- backend/src/models/recipeModel.js
+++ backend/src/models/recipeModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const recipeIngredient = require('recipeIngredientModel');
+const recipeIngredient = require('./recipeIngredientModel');
 
 const recipeSchema = new mongoose.Schema({
     name: { type: String, required: true },
