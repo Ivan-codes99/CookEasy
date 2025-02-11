a864c59eb3072f4dd63ead78d0688f2306287196
crivilla <crivilla@gatech.edu>
2025-02-11 16:12:49 -0500

    ingredient model

--- backend/src/models/ingredientModel.js
+++ backend/src/models/ingredientModel.js
@@ -1,10 +1,13 @@
-// const mongoose = require('mongoose');
-//
-// const ingredientSchema = new mongoose.Schema({
-//         name: { type: String, required: true },
-//         expirationDate: { type: String, required: true},
-//         category: { type: String, required: true },
-// });
-//
-// const Ingredient = mongoose.model('Ingredient', ingredientSchema);
-// module.exports = Ingredient;
+const mongoose = require('mongoose');
+
+const ingredientSchema = new mongoose.Schema({
+        name: { type: String, required: true },
+        category: { type: String, required: true},
+        unit: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        expirationDate: { type: Date, required: true },
+        addedAt: { type: Date, required: true },
+});
+
+const Ingredient = mongoose.model('Ingredient', ingredientSchema);
+module.exports = Ingredient;
