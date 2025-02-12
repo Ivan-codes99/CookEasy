7984ac7d858749c969f0686622b4f4587464040f
crivilla <crivilla@gatech.edu>
2025-02-11 20:35:26 -0500

    replaced logic for Ingredients to be manage in a Map

--- backend/src/controllers/recipeController.js
+++ backend/src/controllers/recipeController.js
@@ -10,8 +10,14 @@ const { v4: uuidv4 } = require('uuid');
  */
 // TODO: send a list of ingredients,
 const getRecipeRecommendation = async (req, res) => {
+
     try {
-        const ingredients = ["tomato", "onion", "garlic", "basil", "rice", "oats", "flour", "baking powder", "baking soda", "yeast", "sugar", 
+        const user = await User.findById(_id);
+        if (!user) return res.status(404).json({ message: "User not found" });
+
+        // TODO: add ingredients from mongoDB
+
+        const ingredients = ["tomato", "onion", "garlic", "basil", "rice", "oats", "flour", "baking powder", "baking soda", "yeast", "sugar",
                             "honey", "olive oil", "vinegar", "black beans", "tomatoes", "canned tuna", "ketchup", "mustard", "salt", "black pepper",
                             "paprika", "oregano", "basil", "cinnamon", "milk", "butter", "cheddar cheese", "mozzarella cheese", "parmesan", "yogurt", "eggs",
                             "apples", "bananas", "chicken breast", "ground beef", "shrimp", "blueberries", "mango", "tortillas", "sliced bread"]; // we don't have to use all ingredients
@@ -40,21 +46,25 @@ const getRecipeRecommendation = async (req, res) => {
  * Add ingredient endpoint
  */
 const addIngredient = async (req, res) => {
-    let { _id, name, category, totalQuantity } = req.body;
+    let { _id, name, category, quantity } = req.body;
 
     try {
         const user = await User.findById(_id);
         if (!user) return res.status(404).json({ message: "User not found" });
 
         // Check if ingredient already exists in kitchen stock
-        const existingIngredient = user.kitchenStock.find(ingredient => ingredient.name === name);
-        if (existingIngredient) {
-            existingIngredient.totalQuantity += totalQuantity;
-            // TODO: update expirationDate logic
-        } else {
-            user.kitchenStock.push({ name, category, totalQuantity });
-        }
+        if (user.kitchenStock.has(name)) {
+            let existingIngredient = user.kitchenStock.get(name);
 
+            // Update quantity
+            existingIngredient.quantity += quantity;
+            // TODO: update expirationDate logic with stock
+        } else {
+            user.kitchenStock.set(name, {
+                category,
+                quantity: quantity
+            });
+        }
         await user.save();
         res.status(200).json({ message: "Ingredient added successfully", kitchenStock: user.kitchenStock });
     } catch (error) {
--- backend/src/models/ingredientModel.js
+++ backend/src/models/ingredientModel.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
-const ingredientStock = require("./ingredientStockModel");
+//const ingredientStock = require("./ingredientStockModel");
 
 const ingredientSchema = new mongoose.Schema({
-        name: { type: String, required: true },
         category: { type: String, required: true},
-        totalQuantity: { type: Number, required: true },
+        quantity: { type: Number, required: true },
         //unit: { type: String, required: true },
         // TODO: implement stock
         //stock: { type: [ingredientStock.Schema]},
-});
+},{ _id: false });
 
 module.exports = ingredientSchema;
