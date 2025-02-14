5c9fd2a3eda8d9612e5a703bb9092db459ded1ce
Ivan Figueroa <Ivanfiggomez@gmail.com>
2025-02-14 13:40:38 -0500

    Added info comment

--- backend/src/routes/recipeRoutes.js
+++ backend/src/routes/recipeRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/recommend', getRecipeRecommendation);
 router.post('/add', addIngredient);
+//* I used patch not delete because the function does not delete the ingredient from the user's kitchenStock it decreases it, even if it reaches 0.
 router.patch('/delete', deleteIngredient); // TODO document and test in Postman
 router.post('/save', saveRecipe);
 
