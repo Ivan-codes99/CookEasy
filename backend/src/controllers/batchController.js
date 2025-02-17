bfd1215b1a6cc700a38aa5dfee17734abe64de19
Ivan Figueroa <Ivanfiggomez@gmail.com>
2025-02-16 19:27:02 -0500

    Created addBatch

--- backend/src/controllers/batchController.js
+++ backend/src/controllers/batchController.js
@@ -1,14 +1,28 @@
-/*
-  TODO //?changeCategoryName
-  TODO 
+const User = require('../models/userModel');
+
+/* 
   TODO addBatch
   TODO deleteBatch (low priority for now)
-  TODO other batch methods (low priority for now)
-  TODO toggleCategoryExcluded
-  TODO toggleIngredientExcluded
-  TODO getEarliestExpiringBatch
-  TODO Add ingredient, category should always be specified, Uncategorized is a category
-  TODO Add expirationDate to a batch, 
-  TODO Implement batch merging logic
-       ?Should user be able to have the same ingredient in different categories?
+  TODO other batch methods (low priority for now 
+  TODO figure out batch merging logic
   */
+
+  //* Not gonna check if category or ingredient exists at least for now, since irl that shouldn't happen
+  const addBatch = async (req, res) => { //We just need _id, category, ingredient
+     let {_id, category, ingredient, batch} = req.body;
+     category = category //have to do this for some reason
+     try {
+       const user = await User.findById(_id);
+       if (!user) return res.status(404).json({message: "User not found"}); //checking user exists
+     
+       user.kitchenStock.category.ingredients.ingredient.batches.push(batch);
+       await user.save();
+       res.status(200).json({ message: `New ${ingredient} batch added to kitchen stock successfully supposedly lol`});
+   
+     } catch (error) {
+       res.status(500).json({ message: error.message});
+     }
+   
+   }
+
+   module.exports = {addBatch};
