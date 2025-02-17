e327eb09f7d3a4edb25fe68cf344df2c52be4012
Ivan Figueroa <Ivanfiggomez@gmail.com>
2025-02-16 19:44:08 -0500

    testing addBatch

--- backend/src/controllers/batchController.js
+++ backend/src/controllers/batchController.js
@@ -10,12 +10,11 @@ const User = require('../models/userModel');
   //* Not gonna check if category or ingredient exists at least for now, since irl that shouldn't happen
   const addBatch = async (req, res) => { //We just need _id, category, ingredient
      let {_id, category, ingredient, batch} = req.body;
-     category = category //have to do this for some reason
      try {
        const user = await User.findById(_id);
        if (!user) return res.status(404).json({message: "User not found"}); //checking user exists
-     
-       user.kitchenStock.category.ingredients.ingredient.batches.push(batch);
+       
+       user.kitchenStock[category].ingredients[ingredient].batches.push(batch);
        await user.save();
        res.status(200).json({ message: `New ${ingredient} batch added to kitchen stock successfully supposedly lol`});
    
