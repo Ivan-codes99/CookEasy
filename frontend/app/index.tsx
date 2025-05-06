fb78c26b0a76f8221eeaef088933470e59a1493a
Ivan <Ivanfiggomez@gmail.com>
2025-05-06 19:03:58 -0400

    updated dependencies

--- frontend/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { useEffect } from "react";
-import { Redirect } from "expo-router";
-
-export default function Index() {
-  // Instantly redirect to /auth when app starts
-  return <Redirect href="/auth" />;
-}
